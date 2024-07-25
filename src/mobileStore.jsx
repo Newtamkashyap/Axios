0328e5bf58dd11d49fc5727a68006d75e150beda
Newtam kashyap <82510515+Newtamkashyap@users.noreply.github.com>
2024-07-25 19:32:36 +0530

    buttons added

--- src/mobileStore.jsx
+++ src/mobileStore.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 export function Mobile() {
-    const [products, setProducts] = useState([{ Name:"", Price: 0, Stock: false }])
+    const [products, setProducts] = useState([{ Name: "", Price: 0, Stock: false }])
 
 
     function AddProducts() {
@@ -13,9 +13,39 @@ export function Mobile() {
             })
 
     }
+
+    function PostProduct() {
+        axios.post("products.json")
+
+            .then(response => {
+                console.log(response.data)
+            })
+
+    }
+    function DeleteProduct() {
+        axios.delete("products.json")
+
+            .then(response => {
+                console.log(response.data)
+            })
+
+    }
+    function PutProduct() {
+        axios.put("products.json")
+
+            .then(response => {
+                console.log(response.data)
+            })
+
+    }
     useEffect(() => {
         AddProducts()
+        // PostProduct()
+        //  DeleteProduct()
+        // PutProduct()
     }, [])
+
+
     return (
         <div className="container-fluid">
             <h2>products Table</h2>
@@ -25,18 +55,23 @@ export function Mobile() {
                         <th>Name</th>
                         <th>Price</th>
                         <th>Stock</th>
+                        <th>Add</th>
+                        <th>Delete</th>
                     </tr>
                 </thead>
                 <tbody>
-
-
                     {
                         products.map(product =>
                             <tr key={product.Name}>
                                 <td>{product.Name}</td>
                                 <td>{product.Price}</td>
                                 <td>{(product.Stock === true) ? "Available" : "OutOfStock"}</td>
-
+                                <td >
+                                    <button className="bi bi-plus-square-fill bg bg-danger"></button>
+                                </td>
+                                <td>
+                                    <button className="bi bi-trash-fill bg bg-danger"></button>
+                                </td>
                             </tr>
                         )
                     }
